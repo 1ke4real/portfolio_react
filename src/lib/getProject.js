1cab707b6b66e4b1b0653b019832eb195e7ceddd
Mike Leman <mikeeleman@gmail.com>
2023-10-16 20:52:21 +0200

    remove fs

--- src/lib/getProject.js
+++ src/lib/getProject.js
@@ -1,12 +1,15 @@
-import fs from 'vite-plugin-fs/browser';
-
 export const getProject = async () => {
     const projects = []
-    const files = await fs.readdir("./src/projects")
+    const files = ["pokedex.json", "filmReact.json", "filmNative.json"]
     for (const file of files) {
-        const fileContent = await fetch(`./src/projects/${file}`)
-        const fileContentJson = await fileContent.json()
-        projects.push(fileContentJson)
+        try {
+            const fileContent = await fetch(`./src/projects/${file}`);
+            const fileContentJson = await fileContent.json();
+            projects.push(fileContentJson);
+        } catch (error) {
+            console.error(`Erreur lors de la récupération de ${file}:`, error);
+        }
     }
+
     return projects
 }
