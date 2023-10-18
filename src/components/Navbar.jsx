466a010b745c4b51c7dfd33bac90919142ceaee3
Mike Leman <mikeeleman@gmail.com>
2023-10-18 14:43:33 +0200

    images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-
+import profil from "../assets/images/profil.png"
 export const Navbar = () => {
     let Links = [
         {name: "ACCEUIL", link: "#home"},
@@ -11,16 +11,10 @@ export const Navbar = () => {
 
     return (
         <div className='shadow-md w-full fixed top-0 left-0'>
-            <div className='md:flex items-center justify-between bg-[#262626] py-4 md:px-10 px-7'>
+            <div className='md:flex items-center justify-between bg-black py-4 md:px-10 px-7'>
                 {/* logo section */}
                 <div className='font-bold text-2xl cursor-pointer flex items-center gap-1'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                         stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round"
-                              d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"/>
-                    </svg>
-
-                    <span>Mike LEMAN</span>
+                    <img src={profil} alt="" className="h-10 rounded-full"/>
                 </div>
                 {/* Menu icon */}
                 <div onClick={() => setOpen(!open)} className='absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7'>
@@ -34,14 +28,13 @@ export const Navbar = () => {
                                 <path strokeLinecap="round" strokeLinejoin="round"
                                       d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
                             </svg>
-
                     }
                 </div>
                 {/* linke items */}
-                <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-[#262626] md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-12' : 'top-[-490px]'}`}>
+                <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-black md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-12' : 'top-[-490px]'}`}>
                     {
-                        Links.map((link) => (
-                            <li className='md:ml-8 md:my-0 my-7 font-semibold'>
+                        Links.map((link , key) => (
+                            <li className='md:ml-8 md:my-0 my-7 font-semibold' key={key}>
                                 <a href={link.link}
                                    className=' hover:text-blue-400 duration-500'>{link.name}</a>
                             </li>))
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,8 +14,11 @@ export const Projects = () => {
     }, []);
     return (
         <div className="flex flex-wrap gap-5 justify-center">
-            {projects.map((project) => (<div key={project.title} className=" flex-1 card m-10 p-10 text-center">
-                <h1>{project.title}</h1>
+            {projects.map((project) => (<div key={project.title} className=" flex-1 card mx-5 my-10 p-10 text-center rounded">
+                <h1 className="font-bold text-2xl uppercase">{project.title}</h1>
+                <img src={project.image} alt="" className="w-full h-60 object-cover rounded my-2"/>
+                <a href={project.link} className="button">Voir le projet</a>
+                <a href={project.github} className="button">Voir le code</a>
             </div>))}
 
         </div>
