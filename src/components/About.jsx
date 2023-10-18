c248070fcb5c5118d2699f68d3b9bf86dcb9cc71
Mike Leman <mikeeleman@gmail.com>
2023-10-18 12:44:14 +0200

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,11 @@ export const About = () => {
         <>
             <div className="p-5" id="about">
                 <h1 className="uppercase text-3xl font-bold">à propos</h1>
+                <p className="text-lg mt-5">
+                    Je m'appelle Mike Leman, je suis un jeune développeur passionné. Mon parcours dans le
+                    développement a commencé dès le collège, et depuis lors, j'ai acquis des compétences en autodidacte
+                    et exploré de nouvelles technologies pour repousser les limites de mes connaissances.
+                </p>
             </div>
         </>
     )
